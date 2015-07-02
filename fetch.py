f1d1c42e212bddf80cb8c6212acd2e3d7cfe0a3e
Yifan Cao <yifanc@google.com>
2015-07-02 13:53:49 -0700

    lint the code

--- fetch.py
+++ fetch.py
@@ -15,27 +15,27 @@ cities = []
 result = Counter()
 
 def fetch_kfc(cityname):
-	values = {'cname' : cityname, 'pageIndex' : '1', 'pageSize' : '1' }
-	data = urllib.parse.urlencode(values)
-	data = data.encode('utf-8') # data should be bytes
-	req = urllib.request.Request(BASE_URL, data)
-	req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
-	with urllib.request.urlopen(req) as response:
-	   myres = json.loads(response.read().decode("utf-8"))
-	   return myres['Table'][0]['rowcount']
+    values = {'cname' : cityname, 'pageIndex' : '1', 'pageSize' : '1' }
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8') # data should be bytes
+    req = urllib.request.Request(BASE_URL, data)
+    req.add_header("Content-Type","application/x-www-form-urlencoded;charset=utf-8")
+    with urllib.request.urlopen(req) as response:
+        myres = json.loads(response.read().decode("utf-8"))
+        return myres['Table'][0]['rowcount']
 
 def fetch_all_cities():
-	with open(INPUT_FILE, 'r') as f:
-		cities = f.read().split(',')
+    with open(INPUT_FILE, 'r') as f:
+        cities = f.read().split(',')
 
-	with open(OUTPUT_FILE, 'w', encoding='utf8') as outfile:
-		for single_city in cities:
-			if(single_city):
-				city_count = fetch_kfc(single_city)
-				result[single_city] = city_count
-				outfile.write(single_city + ',')
-				outfile.write(str(city_count) + '\n')
-				outfile.flush()
-				print(result)
+    with open(OUTPUT_FILE, 'w', encoding='utf8') as outfile:
+        for single_city in cities:
+            if(single_city):
+                city_count = fetch_kfc(single_city)
+                result[single_city] = city_count
+                outfile.write(single_city + ',')
+                outfile.write(str(city_count) + '\n')
+                outfile.flush()
+                print(result)
 
 fetch_all_cities()
